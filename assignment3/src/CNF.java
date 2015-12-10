9f83dbf8e529056adef9c5eca9da51a09df12672
loki <growingdever@gmail.com>
2015-12-10 14:19:29 +0900

    imply 구현. clause 안에 또 다시 cnf가 나온다거나 하는건 안 된다....

--- assignment3/src/CNF.java
+++ assignment3/src/CNF.java
@@ -91,4 +91,90 @@ public class CNF {
         return result;
     }
 
+    public static CNF convertFromImply(CNF cnf1, CNF cnf2) {
+        if (cnf1.onlyOneClause() && cnf2.onlyOneClause()) {
+            // assume cnf1 is DNF and cnf2 is DNF
+            CNF newCNF = new CNF();
+
+            Clause clause1 = cnf1.get(0);
+            Clause clause2 = cnf2.get(0);
+
+            for (int i = 0; i < clause1.size(); i ++) {
+                Clause newClause = new Clause();
+                newClause.add(
+                        new PLWumpusWorldSymbol(clause1.get(i), !clause1.get(i).isNegation)
+                );
+                for (int j = 0; j < clause2.size(); j ++) {
+                    newClause.add(new PLWumpusWorldSymbol(clause2.get(j)));
+                }
+                newCNF.add(newClause);
+            }
+
+            return newCNF;
+        } else if (cnf1.onlyOneClause() && !cnf2.onlyOneClause()) {
+            // assume cnf1 is DNF and cnf2 is CNF
+            CNF newCNF = new CNF();
+            Clause clause1 = cnf1.get(0);
+
+            for (int i = 0; i < clause1.size(); i ++) {
+                for (int j = 0; j < cnf2.size(); j ++) {
+                    Clause clause2 = cnf2.get(j);
+                    for (int k = 0; k < clause2.size(); k ++) {
+                        Clause newClause = new Clause();
+
+                        PLWumpusWorldSymbol symbol1 = new PLWumpusWorldSymbol(clause1.get(i), !clause1.get(i).isNegation);
+                        PLWumpusWorldSymbol symbol2 = new PLWumpusWorldSymbol(clause2.get(k));
+
+                        newClause.add(symbol1);
+                        newClause.add(symbol2);
+
+                        newCNF.add(newClause);
+                    }
+                }
+            }
+
+            return newCNF;
+        } else if (!cnf1.onlyOneClause() && cnf2.onlyOneClause()) {
+            CNF newCNF = new CNF();
+            Clause newClause = new Clause();
+
+            for (int i = 0; i < cnf1.size(); i ++) {
+                Clause clause1 = cnf1.get(i);
+                for (int j = 0; j < clause1.size(); j ++) {
+                    newClause.add(new PLWumpusWorldSymbol(clause1.get(j), !clause1.get(j).isNegation));
+                }
+            }
+
+            Clause clause2 = cnf2.get(0);
+            for (int j = 0; j < clause2.size(); j ++) {
+                newClause.add(new PLWumpusWorldSymbol(clause2.get(j)));
+            }
+
+            newCNF.add(newClause);
+            return newCNF;
+        } else {
+            CNF newCNF = new CNF();
+
+            for (int i = 0; i < cnf2.size(); i ++) {
+                Clause clause2 = cnf2.get(i);
+
+                Clause newClause = new Clause();
+                newClause.add(clause2.get(0));
+
+                for (int j = 0; j < cnf1.size(); j ++) {
+                    Clause clause1 = cnf1.get(j);
+                    newClause.add(new PLWumpusWorldSymbol(clause1.get(0), !clause1.get(0).isNegation));
+                }
+
+                newCNF.add(newClause);
+            }
+
+            return newCNF;
+        }
+    }
+
+    public boolean onlyOneClause() {
+        return this.size() == 1;
+    }
+
 }
